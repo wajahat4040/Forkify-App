89c05215fe50a69d24e16a52aa0f094043f434aa
wajahat4040 <wajahatkhan4040@gmail.com>
2021-12-19 18:33:20 +0500

    recipe-fork-app

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -18,18 +18,22 @@ export const RecipeCard = StyleSheet.create({
     alignItems: 'center',
     marginLeft: RECIPE_ITEM_MARGIN,
     marginTop: 20,
-    width: (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) / recipeNumColums,
+    width:
+      (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) /
+      recipeNumColums,
     height: RECIPE_ITEM_HEIGHT + 75,
     borderColor: '#cccccc',
     borderWidth: 0.5,
-    borderRadius: 15
+    borderRadius: 15,
   },
   photo: {
-    width: (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) / recipeNumColums,
+    width:
+      (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) /
+      recipeNumColums,
     height: RECIPE_ITEM_HEIGHT,
     borderRadius: 15,
     borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0
+    borderBottomRightRadius: 0,
   },
   title: {
     flex: 1,
@@ -43,6 +47,6 @@ export const RecipeCard = StyleSheet.create({
   },
   category: {
     marginTop: 5,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
